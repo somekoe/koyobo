38b0969a2553539e13a11134757d7c66cbbb5179
somekoe <77117222+somekoe@users.noreply.github.com>
2022-04-15 01:40:19 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "2b7510be-3510-4d8b-84c8-f30f1211b46c",
                         "alterId": 0
                     }
                 ],
